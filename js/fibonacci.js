3961c6854ad912bfe33dbd24227ce4da13a4a6a2
JohnOK <john.ok.esp@gmail.com>
2022-06-05 03:21:52 +0200

    Added JS version of fibonacci.

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,6 +1,12 @@
+// Recursive version
 const fibonacci = (num) => {
-    //TEST
-    
+    if (num === 0) {
+     return 0;   
+    } else if (num === 1) {
+        return 1;
+    }
+
+    return fibonacci(num - 1) + fibonacci(num - 2);    
 }
 
 module.exports = {fibonacci}
